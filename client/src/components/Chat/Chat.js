5aef11e8b5b5670367895893ca708ede7663f22c
baderbintahir <badarbintahir@gmail.com>
2020-06-01 21:07:06 +0500

    Completed the Project

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -4,6 +4,8 @@ import io from 'socket.io-client'
 
 import InfoBar from '../InfoBar/InfoBar'
 import Input from '../Input/Input'
+import Messages from '../Messages/Messages'
+import TextContainer from '../TextContainer/TextContainer'
 
 import './Chat.css'
 
@@ -12,6 +14,7 @@ let socket;
 const Chat = ({ location }) => {
   const [name, setName] = useState('');
   const [room, setRoom] = useState('');
+  const [users, setUsers] = useState('');
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
   const ENDPOINT = 'localhost:5000';
@@ -39,6 +42,10 @@ const Chat = ({ location }) => {
     socket.on('message', (message) => {
       setMessages([...messages, message])
     })
+
+    socket.on('roomData', ({ users}) => {
+      setUsers(users);
+    })
   }, [messages])
 
   const sendMessage = (event) => {
@@ -55,13 +62,10 @@ const Chat = ({ location }) => {
     <div className="outerContainer">
       <div className="container">
         <InfoBar room={room} />
-
-        {/* <input
-         value={message}
-         onChange={(event) => setMessage(event.target.value)} 
-         onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null}
-        /> */}
+        <Messages messages={messages} name={name} />
+        <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
       </div>
+      <TextContainer users={users}/>
     </div>
   )
 }
